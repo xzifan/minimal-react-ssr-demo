2b16d0939a457d87113635d2ab10dba59d849f10
xzifan <xiao@zifan.dev>
2021-09-17 22:43:20 +0800

    remove build files

--- webpack.conf.js
+++ webpack.conf.js
@@ -37,7 +37,7 @@ module.exports = {
         ]
     },
     output: {
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, './build'),
         filename: '[name].bundle.js',
     },
     plugins: [
