f88cfd6fc9dd787525e4a8449a426a8a630b1984
xzifan <xiao@zifan.dev>
2021-10-30 19:12:32 +0800

    fix: Nav

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,7 +1,7 @@
 import { Nav } from "@alifd/next";
 import Link from "next/link";
-export default () => 
-  <Nav
+export default function Navigation(){ 
+  return <Nav
     className="top-nav"
     type="line"
     direction="hoz"
@@ -11,3 +11,4 @@ export default () =>
     <Nav.Item key="home"><Link href="/">Home</Link></Nav.Item>
     <Nav.Item key="video"><Link href="/video">Videos</Link></Nav.Item>
   </Nav>
+}
