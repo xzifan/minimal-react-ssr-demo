c823530952680e237841fe7f73b41f39d9b8489f
xzifan <xiao@zifan.dev>
2021-10-30 17:08:15 +0800

    fix webpack warning

--- pages/video/index.js
+++ pages/video/index.js
@@ -2,7 +2,7 @@
 import Navigation from "../../components/Navigation";
 import { useRouter } from 'next/router';
 
-export default function Video(){
+export default function Home(){
   const router = useRouter()
   console.log(router)
   return (<div className="app_container video_page">
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-function App() {
-	return <div>
-		test
-	</div>
-}
-
-ReactDOM.render(<App />, document.getElementById('app'))
